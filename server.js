de3a25d6c36d266f5af05e1fedb82571d8147895
Adil <89954695+programest@users.noreply.github.com>
2025-07-16 21:29:25 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const { run } = require('./index.js'); // Импортируем функцию run
-
+const { run } = require('./index.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({ origin: 'https://automation-wb-m5rk.vercel.app' }));
 app.use(express.static('.'));
+app.use(express.json());
 
 app.post('/run-script', async (req, res) => {
   try {
-    await run(); // Вызываем функцию run из index.js
+    const { searchQuery, pageStart, pageEnd, question } = req.body;
+    await run(searchQuery, pageStart, pageEnd, question);
     res.status(200).send('Скрипт завершен');
   } catch (error) {
     console.error('Ошибка выполнения скрипта:', error);
