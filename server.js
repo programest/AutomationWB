6a9bc5401c9ec694fd990bad6afb4fcecece9046
Adil <89954695+programest@users.noreply.github.com>
2025-07-16 21:28:44 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require('express');
-const { exec } = require('child_process');
+const cors = require('cors');
+const { run } = require('./index.js'); // Импортируем функцию run
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('.')); // Обслуживание статических файлов (index.html, results.json)
+app.use(cors({ origin: 'https://automation-wb-m5rk.vercel.app' }));
+app.use(express.static('.'));
 
-app.post('/run-script', (req, res) => {
-  exec('node index.js', (error, stdout, stderr) => {
-    if (error) {
-      console.error('Ошибка выполнения скрипта:', stderr);
-      return res.status(500).send('Ошибка выполнения скрипта');
-    }
-    console.log('Скрипт выполнен:', stdout);
+app.post('/run-script', async (req, res) => {
+  try {
+    await run(); // Вызываем функцию run из index.js
     res.status(200).send('Скрипт завершен');
-  });
+  } catch (error) {
+    console.error('Ошибка выполнения скрипта:', error);
+    res.status(500).send('Ошибка выполнения скрипта');
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Сервер запущен на http://localhost:${port}`);
+  console.log(`Сервер запущен на порту ${port}`);
 });
